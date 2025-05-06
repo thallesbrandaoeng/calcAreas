ed3cbda476bff52aa1fe1f52b68085c4e36b96a4
thalles <thalles.bran@gmail.com>
2025-05-05 22:00:16 -0300

    Add funcao area circulo

--- areasGeom.py
+++ areasGeom.py
@@ -1,5 +1,13 @@
+from math import pi
+
 def area_ret(ladoA, ladoB):
 
     area = ladoA * ladoB
 
-    return
+    return area
+
+def area_circ(diam):
+
+    area = (pi * diam**2 ) / 4
+
+    return area
